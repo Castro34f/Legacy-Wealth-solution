bb5f9ee51e5899f641a60df4a37b7ef3b25a5ec5
Castro34f <yanechcastro@gmail.com>
2025-09-17 15:31:05 +0100

    Ready for deployment: clean code, real data, admin controls, Tawk.to live chat

--- app.py
+++ app.py
@@ -353,7 +353,5 @@ def deposit():
 def withdraw():
     return render_template("withdraw.html")
 
-
 if __name__ == "__main__":
     app.run(debug=True)
-app.run(host="0.0.0.0", port=5000, debug=True)
